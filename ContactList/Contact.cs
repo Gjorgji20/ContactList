f671c4f041137a55bd34faab346da1a59fb688f7
Gjorgji <Gjorgji Si>
2023-05-02 21:04:54 +0200

    Add project files.

---
+++ ContactList/Contact.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    public class Contact
+    {
+        private string _name;
+        public string Surname { get;set; }
+        public string Address { get; set; }
+        public List<string> PhoneNumbers { get;set; }
+
+        public Contact() { }
+
+        public Contact(string name, string surname, string address, List<string> phoneNumbers)
+        {
+            _name = name.Length > 4 && name.Length < 10 ? name : throw new Exception("Inavlid name");
+            #region ctor 
+            Surname = surname;
+            Address = address;
+
+            if (phoneNumbers.Count <= 5)
+            {
+                bool flag = true;
+
+                for (int i = 0; i < phoneNumbers.Count; i++)
+                {
+                    if (!(phoneNumbers[i].Count() == 9 && (phoneNumbers[i].StartsWith("071") || phoneNumbers[i].StartsWith("070") ||
+                        phoneNumbers[i].StartsWith("072") || phoneNumbers[i].StartsWith("075") || phoneNumbers[i].StartsWith("076") ||
+                        phoneNumbers[i].StartsWith("077") || phoneNumbers[i].StartsWith("078"))))
+                    {
+                       flag= false;
+                    }
+
+                }
+                if (flag)
+                {
+                    //inciijalizaicija pri konstruktor so parametri
+                    PhoneNumbers=phoneNumbers;
+                }
+
+            }
+            else
+            {
+                throw new Exception("List has more than 5 elemtns");
+            }
+            #endregion
+        }
+
+        public string getName()
+        {
+            //return Name;
+            return _name;
+        }
+        public List<string> getNumbers()
+        {
+            string savePosition = "";
+            //sortiranje na niza
+            for(int i=0;i<PhoneNumbers.Count-1;i++)
+            {
+                for(int j=1;j<PhoneNumbers.Count;j++) 
+                {
+                    if (PhoneNumbers[i].CompareTo(PhoneNumbers[j])>0)
+                    {
+                        savePosition= PhoneNumbers[i];
+                        PhoneNumbers[i] = PhoneNumbers[j];
+                        PhoneNumbers[j]= savePosition;
+                    }
+                }
+            }
+            return PhoneNumbers;
+        }
+
+        public string addNumber(string number)
+        {
+            if(PhoneNumbers.Count<5)
+            {
+                PhoneNumbers.Add(number);
+                return "Added number";
+            }else
+
+            {
+                return "Not added number";
+            }
+        }
+        public string Pecati()
+        {
+            string numbers = "";
+            foreach(var number in PhoneNumbers)
+            {               
+                numbers += $"Number {PhoneNumbers.IndexOf(number)}: {number}";
+            }
+
+            string message = $"Name: {_name}, Surname: {Surname}, Address: {Address}, Numbers: {numbers}";
+
+            return message;
+        }
+
+
+    }
+}
---
+++ ContactList/PhoneBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    public class PhoneBook
+    {
+        public List<Contact> Contacts { get; set; }
+        
+        public PhoneBook() { }
+
+        public PhoneBook(List<Contact> contacts)
+        {
+            Contacts = contacts;
+        }
+        public void addContact(Contact contact)
+        {
+            if(Contacts.Count > 250) 
+            {
+                foreach(Contact con in Contacts)
+                {
+                    if(con.getName().Equals(contact.getName()))
+                    {
+                        throw new Exception("The name exists");
+                    }
+                    else
+                    {
+                        Contacts.Add(con);
+                        Console.WriteLine("The contact was added");
+                    }    
+                }
+            }
+            else
+            {
+                throw new Exception("full list");
+            }    
+        }
+    }
+}
